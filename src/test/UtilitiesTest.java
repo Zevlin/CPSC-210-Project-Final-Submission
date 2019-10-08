2b8ba4d74e24769a1bb9e3b062b005dee158d139
Dhaher <dhaher24@hotmail.com>
2019-10-07 18:20:39 -0700

    Lab 5: - improved coverage

--- src/test/UtilitiesTest.java
+++ src/test/UtilitiesTest.java
@@ -3,9 +3,12 @@ import model.Rest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.io.File;
 import utilities.*;
 
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class UtilitiesTest {
@@ -29,5 +32,29 @@ public class UtilitiesTest {
         assertEquals("Rest", logger.getStampName());
         assertEquals(0, logger.getStampDuration());
         assertEquals(day.toString(), logger.getStampStart());
+    }
+
+    @Test
+    public void saveDataTest() throws Exception {
+        File f = new File(".\\Data\\timeLog.csv");
+        if (f.exists()) {
+            f.delete();
+        }
+        assertFalse(f.exists());
+        logger.saveData(new ArrayList(){});
+        assertTrue(f.exists());
+    }
+
+    @Test
+    public void loadDataTest() throws Exception {
+        File f = new File(".\\Data\\timeLog.csv");
+        if (f.exists()) {
+            f.delete();
+        }
+        assertFalse(f.exists());
+        PrintWriter out = new PrintWriter(new FileOutputStream((f)));
+        out.println("test");
+        out.close();
+        assertEquals("test", logger.loadData().get(0));
     }
 }
