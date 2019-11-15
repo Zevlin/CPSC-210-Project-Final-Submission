b6e4a0ca4ba93a19a37ad92628be3fd49fd1f7b8
Dhaher <dhaher24@hotmail.com>
2019-11-15 15:29:24 -0800

    Lab 11: added logo to top bar and greeting/tasksTitle/tasksButtons to side bar.

--- src/main/UI/TimeTrackerApp.java
+++ src/main/UI/TimeTrackerApp.java
@@ -7,10 +7,13 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import network.ReadWebPage;
@@ -28,9 +31,19 @@ public class TimeTrackerApp extends Application implements EventHandler<ActionEv
     // FIELDS
     private static final int WWIDTH = 896;
     private static final int WHEIGHT = 560;
+    private static final double TRANSPARENCY = 1.0;
+    private int topBarHeight = 55;
+    private int sideBarWidth = 195;
     ImageView iv;
+    private int taskBtnWidth = 40;
+    private int taskBtnHeight = 30;
     private Button closeBtn;
     private Button workBtn;
+    private Button travelBtn;
+    private Button eatBtn;
+    private Button hobbyBtn;
+    private Button shopBtn;
+    private Button studyBtn;
     private Button restBtn;
     private TimeLogger timeLogger;
     private Timer timer;
@@ -86,22 +99,6 @@ public class TimeTrackerApp extends Application implements EventHandler<ActionEv
     //EFFECTS: forms the initial layout of the GUI
     public void initializeGUI() {
         System.out.println("form GUI method");
-
-        // current activity elements
-        currentActivityTxt = new Text("-- Current Activity --");
-        startText = new Text("Start: --/--");
-        durationText = new Text("Duration: 00:00:00");
-
-        // new task elements
-        newTaskTxt = new Text("-------  Start New Task  -------");
-        workBtn = new Button("work");
-        workBtn.setOnAction(this);
-        restBtn = new Button("rest");
-        restBtn.setOnAction(this);
-
-        // activity log
-        dateText = new Text(new SimpleDateFormat("dd/mm/yyyy").format(new Date()));
-        dateUnderlineTxt = new Text("-----------------");
     }
 
     // MODIFIES: this
@@ -112,11 +109,13 @@ public class TimeTrackerApp extends Application implements EventHandler<ActionEv
         HBox bodyWrapper = makeBodyWrapper(topBar);
 
         VBox innerWrapperBox = new VBox();
+        innerWrapperBox.setStyle("-fx-background-color: transparent;");
         innerWrapperBox.setMinHeight(WHEIGHT);
         innerWrapperBox.setMinWidth(WWIDTH);
         innerWrapperBox.getChildren().addAll(topBar, bodyWrapper);
 
         HBox wrapperBox = new HBox();
+        wrapperBox.setStyle("-fx-background-color: transparent;");
         wrapperBox.getChildren().add(innerWrapperBox);
         wrapperBox.minWidth(WWIDTH);
         wrapperBox.minHeight(WHEIGHT);
@@ -130,7 +129,7 @@ public class TimeTrackerApp extends Application implements EventHandler<ActionEv
         bodyWrapper.setMaxHeight(WHEIGHT - topBar.getHeight());
         bodyWrapper.setMinWidth(WWIDTH);
         bodyWrapper.setMaxWidth(WWIDTH);
-        bodyWrapper.setStyle("-fx-background-color: rgba(46, 45, 71, 1.0);");
+        bodyWrapper.setStyle("-fx-background-image: url(\"images/bg.png\");");
 
         VBox sideBar = makeSideBar(bodyWrapper);
 
@@ -139,25 +138,127 @@ public class TimeTrackerApp extends Application implements EventHandler<ActionEv
     }
 
     private VBox makeSideBar(HBox bodyWrapper) {
+        HBox greeting = new HBox();
+        greeting.setMinWidth(45);
+        greeting.setMaxWidth(45);
+        greeting.setMinHeight(35);
+        greeting.setMaxHeight(35);
+        greeting.setStyle("-fx-background-image: url(\"images/hello.png\");"
+                + "-fx-background-position: center;"
+                + "-fx-background-repeat: no-repeat;");
+
+        Text hello = new Text("hello  ");
+        hello.setFill(Paint.valueOf("white"));
+
+        Text name = new Text("Dhaher");
+        name.setTextAlignment(TextAlignment.LEFT);
+        name.setFill(Paint.valueOf("white"));
+        name.setWrappingWidth(80);
+
+        HBox greetingWrapper = new HBox();
+        greetingWrapper.setAlignment(Pos.CENTER);
+        greetingWrapper.setMinWidth(sideBarWidth);
+        greetingWrapper.setMaxWidth(sideBarWidth);
+        greetingWrapper.setMinHeight(70);
+        greetingWrapper.setMaxHeight(70);
+        greetingWrapper.getChildren().addAll(greeting, hello, name);
+
+        Text tasks = new Text("tasks");
+        tasks.setFill(Paint.valueOf("white"));
+        VBox tasksTitle = new VBox();
+        tasksTitle.setAlignment(Pos.CENTER);
+        tasksTitle.setMinWidth(sideBarWidth);
+        tasksTitle.setMaxWidth(sideBarWidth);
+        tasksTitle.setMinHeight(40);
+        tasksTitle.setMaxHeight(40);
+        tasksTitle.setStyle("-fx-background-image: url(\"images/tasks_noText.png\");"
+                + "-fx-background-position: center;"
+                + "-fx-background-repeat: no-repeat;");
+        tasksTitle.getChildren().add(tasks);
+
+        workBtn = makeTaskButton("work");
+        studyBtn = makeTaskButton("study");
+        travelBtn = makeTaskButton("travel");
+        eatBtn = makeTaskButton("eat");
+        shopBtn = makeTaskButton("shop");
+        hobbyBtn = makeTaskButton("hobby");
+        restBtn = makeTaskButton("rest");
+
+        HBox taskRow1 = new HBox();
+        taskRow1.setPadding(new Insets(0,0,0, 15));
+        taskRow1.setAlignment(Pos.CENTER_LEFT);
+        taskRow1.setMinWidth(sideBarWidth);
+        taskRow1.setMaxWidth(sideBarWidth);
+        taskRow1.setMinHeight(36);
+        taskRow1.setMaxHeight(36);
+        taskRow1.getChildren().addAll(workBtn, studyBtn, travelBtn, eatBtn);
+
+        HBox taskRow2 = new HBox();
+        taskRow2.setPadding(new Insets(0,0,0, 15));
+        taskRow2.setAlignment(Pos.CENTER_LEFT);
+        taskRow2.setMinWidth(sideBarWidth);
+        taskRow2.setMaxWidth(sideBarWidth);
+        taskRow2.setMinHeight(36);
+        taskRow2.setMaxHeight(36);
+        taskRow2.getChildren().addAll(shopBtn, hobbyBtn, restBtn);
+
+        VBox tasksWrapper = new VBox();
+        tasksWrapper.setAlignment(Pos.CENTER);
+        tasksWrapper.setMinWidth(sideBarWidth);
+        tasksWrapper.setMaxWidth(sideBarWidth);
+        tasksWrapper.setMinHeight(75);
+        tasksWrapper.setMaxHeight(75);
+        tasksWrapper.setStyle("-fx-background-color: rgba(53, 49, 115, " + TRANSPARENCY + ");");
+        tasksWrapper.getChildren().addAll(taskRow1, taskRow2);
+
         VBox sideBar = new VBox();
-        sideBar.setMinWidth(200);
-        sideBar.setMaxWidth(200);
+        sideBar.setAlignment(Pos.TOP_CENTER);
+        sideBar.setMinWidth(sideBarWidth);
+        sideBar.setMaxWidth(sideBarWidth);
         sideBar.setMinHeight(bodyWrapper.getHeight());
-        sideBar.setStyle("-fx-background-color: rgba(60, 56, 128, 1.0);");
+        sideBar.setStyle("-fx-background-color: rgba(60, 56, 128, " + TRANSPARENCY + ");");
+        sideBar.getChildren().addAll(greetingWrapper, tasksTitle, tasksWrapper);
         return sideBar;
     }
 
+    private Button makeTaskButton(String name) {
+        Button temp = new Button();
+        temp.setMinWidth(taskBtnWidth);
+        temp.setMaxWidth(taskBtnWidth);
+        temp.setMinHeight(taskBtnHeight);
+        temp.setMaxHeight(taskBtnHeight);
+        temp.setStyle("-fx-background-image: url(\"images/" + name + "_active.png\");"
+                + "-fx-border-color: transparent;"
+                + "-fx-border-width: 0;"
+                + "-fx-background-radius: 0;"
+                + "-fx-background-color: transparent;"
+                + "-fx-background-position: center center;"
+                + "-fx-background-repeat: no-repeat;");
+        temp.setOnAction(this);
+        return temp;
+    }
+
     private HBox makeTopBar() {
+        makeCloseBtn();
+
+        VBox logo = new VBox();
+        logo.setMinHeight(31);
+        logo.setMaxHeight(31);
+        logo.setMinWidth(182);
+        logo.setMaxWidth(182);
+        logo.setStyle("-fx-background-image: url(\"images/logo.png\");" + "-fx-background-repeat: no-repeat;");
+
+        VBox logoBuffer = new VBox();
+        logoBuffer.setMinWidth(650);
+
         HBox topBar = new HBox();
         topBar.setPadding(new Insets(0, 30, 0, 30));
         topBar.setAlignment(Pos.CENTER_RIGHT);
         topBar.setPrefWidth(WWIDTH);
-        topBar.setMinHeight(70);
-        topBar.setMaxHeight(70);
-        String topBarCSS = "-fx-background-color: rgba(67, 63, 145, 1.0);";
-        topBar.setStyle(topBarCSS);
-        makeCloseBtn();
-        topBar.getChildren().add(closeBtn);
+        topBar.setMinHeight(55);
+        topBar.setMaxHeight(55);
+        topBar.setStyle("-fx-background-color: rgba(67, 63, 145, " + TRANSPARENCY + ");");
+        topBar.getChildren().addAll(logo, logoBuffer, closeBtn);
         return topBar;
     }
 
@@ -172,7 +273,6 @@ public class TimeTrackerApp extends Application implements EventHandler<ActionEv
                 + "-fx-border-width: 0;"
                 + "-fx-background-radius: 0;"
                 + "-fx-background-color: transparent;"
-                + "-fx-background-color: rgba(67, 63, 145, 1.0);"
                 + "-fx-background-position: center center;"
                 + "-fx-background-repeat: no-repeat;"
                 + "-fx-background-image: url(\"images/close.png\");");
