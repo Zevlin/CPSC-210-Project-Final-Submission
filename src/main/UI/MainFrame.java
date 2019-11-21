aa4fb5de2f49ea763c9bfa7ed1541614c776765b
Dhaher <dhaher24@hotmail.com>
2019-11-20 16:15:27 -0800

    Lab 11: Body visuals mostly complete. - Implemented timer button functionalty, tot, total time, average time - name input, time log, pie chart pending - button animation pending - spawn animation pending

--- src/main/UI/MainFrame.java
+++ src/main/UI/MainFrame.java
@@ -1,12 +1,20 @@
 package ui;
 
 
+import model.Activity;
+import model.Rest;
+import model.TimeStamp;
+import utilities.TimeLogger;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainFrame {
 
@@ -15,15 +23,21 @@ public class MainFrame {
     private static final int FHEIGHT = 800;
     private int topBarHeight = 80;
     private int sideBarWidth = 275;
+    private int bodyWidth = 1000;
+    private int bodyHeight = 700;
     private int taskBtnWidth = 45;
     private int taskBtnHeight = 45;
     private int taskBtnGap = 5;
     private JFrame window;
     private JPanel topBar;
     private JPanel sideBar;
+    private JPanel body;
     private int topBarAlpha = 255;
     private int sideBarAlpha = 255;
     private String profileName = "Dhaher";
+    private int totTime = 0;
+    private int sidestatsTotalTimeLogged = 0;
+    private int sidestatsAverageTaskLength = 0;
 
     private JButton closeBtn;
     private JButton workBtn = new JButton();
@@ -33,14 +47,37 @@ public class MainFrame {
     private JButton shopBtn = new JButton();
     private JButton hobbyBtn = new JButton();
     private JButton restBtn = new JButton();
+    private JButton activeBtn;
+    private JLabel tot;
+    private JLabel totalTimeLogged;
+    private JLabel averageTaskLength;
+
+    private TimeLogger timeLogger;
 
     // PRIMARY METHODS
-    public MainFrame() throws IOException {
+    public MainFrame(TimeLogger timeLogger) throws IOException {
+        this.timeLogger = timeLogger;
         formWindow();
+        formBody();
         formTopBar();
         formSideBar();
 
         window.setVisible(true);
+    }
+
+    public void update() {
+        totTime = timeLogger.getStampDuration();
+        tot.setText(formatTime(totTime));
+        int tempTTL = 0;
+        int taskDurationSum = 0;
+        int taskCount = 0;
+        for (TimeStamp s : timeLogger.getLogList()) {
+            tempTTL += s.getDuration();
+            taskDurationSum += s.getDuration();
+            taskCount++;
+        }
+        totalTimeLogged.setText(formatTime(tempTTL));
+        averageTaskLength.setText(formatTime(taskDurationSum / taskCount));
     }
 
     // HELPER METHODS
@@ -52,6 +89,7 @@ public class MainFrame {
         window.setUndecorated(true);
         window.setResizable(false);
         window.setLayout(null);
+        window.setLocationRelativeTo(null);
 //        File f = new File("src/main/images/bg.png");
 //        System.out.println(f.getCanonicalPath());
         ImageIcon img = new ImageIcon("src/main/images/bg.png");
@@ -81,6 +119,12 @@ public class MainFrame {
         closeBtn.setFocusPainted(false);
         closeBtn.setIcon(closeImg);
         topBar.add(closeBtn);
+        closeBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(1);
+            }
+        });
 
         window.add(topBar);
     }
@@ -178,7 +222,67 @@ public class MainFrame {
 
         tasksList.add(Box.createRigidArea(new Dimension(0, 10)));
 
+        buttonListeners();
+
         return tasksList;
+    }
+
+    private void buttonListeners() {
+        workBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formatTaskButton(activeBtn, false);
+                formatTaskButton(workBtn, true);
+            }
+        });
+
+        studyBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formatTaskButton(activeBtn, false);
+                formatTaskButton(studyBtn, true);
+            }
+        });
+
+        travelBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formatTaskButton(activeBtn, false);
+                formatTaskButton(travelBtn, true);
+            }
+        });
+
+        eatBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formatTaskButton(activeBtn, false);
+                formatTaskButton(eatBtn, true);
+            }
+        });
+
+        shopBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formatTaskButton(activeBtn, false);
+                formatTaskButton(shopBtn, true);
+            }
+        });
+
+        hobbyBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formatTaskButton(activeBtn, false);
+                formatTaskButton(hobbyBtn, true);
+            }
+        });
+
+        restBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formatTaskButton(activeBtn, false);
+                formatTaskButton(restBtn, true);
+            }
+        });
     }
 
     private void formatTaskButton(JButton btn, Boolean active) {
@@ -190,6 +294,12 @@ public class MainFrame {
         btn.setBorderPainted(false);
         btn.setFocusPainted(false);
         if (active) {
+            activeBtn = btn;
+            if (btn.getName().equals("rest")) {
+                timeLogger.newRest(new Rest(btn.getName()));
+            } else {
+                timeLogger.newActivity(new Activity(btn.getName(), btn.getName()));
+            }
             img = new ImageIcon("src/main/images/" + btn.getName() + "_active.png");
         } else {
             img = new ImageIcon("src/main/images/" + btn.getName() + "_inactive.png");
@@ -197,27 +307,77 @@ public class MainFrame {
         btn.setIcon(img);
     }
 
+    private void formBody() {
+        body = new JPanel();
+        body.setOpaque(false);
+        body.setBorder(new EmptyBorder(topBarHeight + 5,sideBarWidth + 5,0,0));
+        body.setSize(FWIDTH, FHEIGHT);
+        body.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+
+
+        ImageIcon totBG = new ImageIcon("src/main/images/totBG.png");
+        tot = new JLabel();
+        tot.setIcon(totBG);
+        tot.setFont(new Font("Century Gothic", Font.PLAIN, 30));
+        tot.setForeground(new Color(44, 82, 115, 255));
+        tot.setText("<html><p>" + formatTime(totTime) + "</p></html>");
+        tot.setHorizontalTextPosition(JLabel.CENTER);
+
+        ImageIcon timeloggedBG = new ImageIcon("src/main/images/timeloggedBG.png");
+        totalTimeLogged = new JLabel();
+        totalTimeLogged.setIcon(timeloggedBG);
+        totalTimeLogged.setFont(new Font("Century Gothic", Font.PLAIN, 30));
+        totalTimeLogged.setForeground(new Color(98, 183, 104, 255));
+        totalTimeLogged.setText("<html><p>" + formatTime(sidestatsTotalTimeLogged) + "</p></html>");
+        totalTimeLogged.setHorizontalTextPosition(JLabel.CENTER);
+
+        ImageIcon averagetasklengthBG = new ImageIcon("src/main/images/averagetasklengthBG.png");
+        averageTaskLength = new JLabel();
+        averageTaskLength.setIcon(averagetasklengthBG);
+        averageTaskLength.setFont(new Font("Century Gothic", Font.PLAIN, 30));
+        averageTaskLength.setForeground(new Color(210, 102, 49, 255));
+        averageTaskLength.setText("<html><p>" + formatTime(sidestatsAverageTaskLength) + "</p></html>");
+        averageTaskLength.setHorizontalTextPosition(JLabel.CENTER);
+
+        ImageIcon sidestatsEmptyBG = new ImageIcon("src/main/images/emptyBG.png");
+        JLabel sideStatsEnd = new JLabel("", sidestatsEmptyBG, JLabel.LEFT);
+
+
+        ImageIcon timelogBG = new ImageIcon("src/main/images/timelogBG.png");
+        JLabel timelog = new JLabel("", timelogBG, JLabel.LEFT);
+
+        ImageIcon piechartBG = new ImageIcon("src/main/images/piechartBG.png");
+        JLabel piechart = new JLabel("", piechartBG, JLabel.LEFT);
+        body.add(tot);
+        body.add(totalTimeLogged);
+        body.add(averageTaskLength);
+        body.add(sideStatsEnd);
+//        body.add(timelog);
+//        body.add(piechart);
+        window.add(body);
+    }
+
     private String formatTime(int n) {
         String timerTextStr = "";
         //Set hours if hrs are 10 or over
         if (n / 3600 >= 10) {
-            timerTextStr += n / 3600 + ":";
+            timerTextStr += n / 3600 + "h ";
         } else {
-            timerTextStr += "0" + n / 3600 + ":";
+            timerTextStr += "" + n / 3600 + "h ";
         }
         n -= n / 3600 * 3600;   // get rid of the hours we already processed
         //Set minutes if mins are 10 or over
         if (n / 60 >= 10) {
-            timerTextStr += n / 60 + ":";
+            timerTextStr += n / 60 + "m";
         } else {
-            timerTextStr += "0" + n / 60 + ":";
+            timerTextStr += "" + n / 60 + "m ";
         }
         //n -= n / 60 * 60;   // get rid of the minutes we already processed
         //Set hours if hrs are 10 or over
         if (n - (n / 60 * 60) >= 10) {
-            timerTextStr += n - (n / 60 * 60);
+            timerTextStr += n - (n / 60 * 60) + "s";
         } else {
-            timerTextStr += "0" + (n - (n / 60 * 60));
+            timerTextStr += "" + (n - (n / 60 * 60)) + "s";
         }
         return timerTextStr;
     }   // End of formatTime()
